b12501cae105fc0caeb4e1bb7ff1091fa3bb991b
BlasBarbosa <91706860+BlasBarbosa@users.noreply.github.com>
2022-02-01 17:54:52 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+// Componentes
+import LocationInfo from './LocationInfo/LocationInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <LocationInfo />
+    </div>
+  );
+}
+
+export default App;
